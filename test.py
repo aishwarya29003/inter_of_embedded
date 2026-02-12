b5d65ff06927ffab02e430773ec39ce24509e5aa
Aishwarya s <gowdaaishu848@gmail.com>
2026-02-12 18:04:05 +0530

    student detail

---
+++ test.py
@@ -0,0 +1,9 @@
+name= input("enter your name:")
+usn= input("enter your usn:")
+sem=input("enter your sem:")
+college=input("enter your college:")
+
+print("name",name)
+print("usn",usn)
+print("sem",sem)
+print("college",college)
